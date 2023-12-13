5948bcbd1efeabad94ce3ff2716b259ae775c45a
SarthakHatwar <137857501+SarthakHatwar@users.noreply.github.com>
2023-12-13 17:30:23 +0530

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// Define a Mongoose Schema for the Product model
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   quantity: { type: Number, required: true },
